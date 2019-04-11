041eaef9729549914fa78f01b47d4a695fc2ad71
Nicholas Monk <nickamonk@gmail.com>
2019-04-11 08:16:30 +0000

    letters to cypher

--- Rotation_cypher_encrypt.c
+++ Rotation_cypher_encrypt.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
 
+int main (){
+    ch[] = "this is the words"; //string of letters
+    
+    int n = 3; //the key is a shift by 3
+    char N;
+    char ch;
+    while(ch >=1) {   
+        for (ch = 'A'; ch <= 'Z'; ++ch) {
+        int k = ch;
+        }
+        for (ch = 'a'; ch <= 'z'; ++ch) {
+        int k = ch;
+        }
+    
+    N = (k + n)%26;
+           
+    for (N = 'A'; N <= 'Z'; N++) {
+    printf("%c\n", ch - 'A')
+}
+    for (N = 'a'; N <= 'z'; N++) {
+    printf("%c\n", ch - 'a');
+}   }
+    }  
